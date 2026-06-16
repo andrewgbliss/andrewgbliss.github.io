ee9d071d22d745e255534123259af0a3e5fd093b
Andrew Bliss <andrewgbliss@gmail.com>
2026-06-16 14:07:25 -0600

    google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +25,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={geistSans.variable}>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-BK1W9GTN3E"
+      />
+      <Script>
+        {`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-BK1W9GTN3E');`}
+      </Script>
       <body className={`${geistMono.variable} antialiased`}>{children}</body>
     </html>
   );
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,7 @@
 import { getAllResumeIds } from "@/lib/resume";
 import { website } from "@/lib/website";
 import type { MetadataRoute } from "next";
+import { getPaths, getTags } from "@/lib/data/articles";
 
 export const revalidate = 5;
 
@@ -25,7 +26,34 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       changeFrequency: "daily",
       priority: 0.8,
     },
+    {
+      url: `${website.url}/articles`,
+      lastModified: new Date(),
+      changeFrequency: "always",
+      priority: 1,
+    },
     ...resumeEntries,
   ];
+
+  const articles = await getPaths();
+  const tags = await getTags();
+
+  articles.forEach((article) => {
+    sitemap.push({
+      url: `${website.url}/articles/${article}`,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    });
+  });
+  tags.forEach((tag) => {
+    sitemap.push({
+      url: `${website.url}/articles?tag=${tag}`,
+      lastModified: new Date(),
+      changeFrequency: "always",
+      priority: 0.5,
+    });
+  });
+
   return sitemap;
 }
